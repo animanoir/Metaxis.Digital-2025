1db910df1314f31be88657001a491e87af48288b
oscaramontiel <etherealflux94@gmail.com>
2025-02-05 16:48:39 -0600

    Add Navbar component, update layout metadata, and create Biblioteca page

---
+++ src/app/biblioteca/page.tsx
@@ -0,0 +1,3 @@
+export default function Library() {
+  return <h1>Biblioteca de metaxis.digital</h1>
+}
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,224 @@
+import React, { useState, useEffect, useCallback } from 'react'
+import * as navbarStyles from '../css/Navbar.module.css'
+import Link from "next/link";
+import boletinSVG from '../images/svg/BoletinSVG.svg'
+import { throttle } from 'lodash'
+
+const Navbar = () => {
+  const tesisWords = [
+    'filosofía',
+    'arte',
+    'fantasmas',
+    'meta',
+    'tesis',
+    'sol',
+    'vida',
+    'muerte',
+    'ser',
+    'meditaciones',
+    'simbolismo',
+    'surrealismo',
+    'conciencia',
+    'antimateria',
+    'materia',
+    'capitalismo',
+    'tú',
+    'ying',
+    'posthumanos',
+    'amor',
+    'blanco',
+    'unos',
+    'existencialismo',
+    'creatividad',
+    'leyendas',
+    'ultraterrenal',
+    'investigación',
+    'estrellas',
+    'existencia',
+    'renacimiento',
+    'alma',
+    'contemplaciones',
+    'analogías',
+    'onirismo',
+    'percepción',
+    'energía oscura',
+    'esencia',
+    'individualismo',
+    'equilibrio',
+    'transhumanismo',
+    'pasión',
+    'claridad',
+    'pluralidad',
+    'sublimación',
+    'catalizador',
+    'espíritu',
+    'navegación',
+    'infinito',
+    'horizonte',
+    'iluminación',
+    'célula',
+    'ecosistema',
+    'destello',
+    'sinestesia',
+    'metáfora',
+    'renovación',
+    'vínculo',
+    'universalidad',
+    'concatenación',
+    'origen',
+    'transición',
+    'expansión',
+    'resiliencia',
+    'introspección',
+    'horizonte',
+    '0',
+  ]
+  const antitesisWords = [
+    'computación',
+    'psicología',
+    'matemáticas',
+    'antitesis',
+    'antimateria',
+    'máquinas',
+    'socialismo',
+    'luna',
+    'antimateria',
+    'nada',
+    'metamorfosis',
+    'yo',
+    'yang',
+    'capitalismo',
+    'amor',
+    'negro',
+    'ceros',
+    'cibernética',
+    'neurociencia',
+    'lógica',
+    'contraposición',
+    'partículas',
+    'robots',
+    'comunismo',
+    'galaxias',
+    'vacío',
+    'transformación',
+    'ego',
+    'dualidad',
+    'tecnocracia',
+    'ontología',
+    'oscuridad',
+    'singularidad',
+    'fragmentación',
+    'confinamiento',
+    'vacío existencial',
+    'desconexión',
+    'finito',
+    'horizonte',
+    'anulación',
+    'partícula',
+    'aislamiento',
+    'obscuridad',
+    'desintegración',
+    'paradoja',
+    'recesión',
+    'análisis',
+    'restrictividad',
+    'limitación',
+    'disolución',
+    'saturación',
+    'contradicción',
+    'fusión',
+    'temporalidad',
+    'dispersión',
+    '1',
+  ]
+
+  const getRandomWord = (words) => {
+    return words[Math.floor(Math.random() * words.length)]
+  }
+
+  const [tesisAntitesis, setTesisAntitesis] = useState({
+    tesis: getRandomWord(tesisWords),
+    antitesis: getRandomWord(antitesisWords),
+  })
+  const [scrollY, setScrollY] = useState(0)
+
+  const updateScrollPosition = useCallback(
+    throttle(() => {
+      setScrollY(window.pageYOffset)
+    }, 50),
+    []
+  )
+
+  useEffect(() => {
+    window.addEventListener('scroll', updateScrollPosition)
+    return () => {
+      window.removeEventListener('scroll', updateScrollPosition)
+    }
+  }, [updateScrollPosition])
+
+  useEffect(() => {
+    let selectedTesis = getRandomWord(tesisWords)
+    let selectedAntitesis = getRandomWord(antitesisWords)
+
+    while (selectedTesis === selectedAntitesis) {
+      selectedAntitesis = getRandomWord(antitesisWords)
+    }
+
+    setTesisAntitesis({ tesis: selectedTesis, antitesis: selectedAntitesis })
+  }, [scrollY])
+
+  return (
+    <nav className={navbarStyles.container}>
+      <div>
+        <Link to="/">
+          <h1 className={navbarStyles.title}>
+            <Link fade to="/">
+              <span className={navbarStyles.antitesis} style={{ fontWeight: 'normal' }}>
+                {' '}
+                {tesisAntitesis.tesis} — {tesisAntitesis.antitesis} |{' '}
+              </span>
+              <span>
+                <b>metaxis.digital</b>
+              </span>{' '}
+            </Link>{' '}
+          </h1>
+        </Link>
+      </div>
+      <ul className={navbarStyles.menulist}>
+        <li>
+          <Link fade to="/Acerca">
+            Acerca
+          </Link>
+        </li>
+        <li>
+          <Link fade to="/Colabora">
+            <b>¡Colabora!</b>
+          </Link>
+        </li>
+        <li>
+          <Link fade to="/Biblioteca">
+            <b>Biblioteca</b>
+          </Link>
+        </li>
+        <li>
+          <Link fade to="/Conceptos">
+            Conceptos
+          </Link>
+        </li>
+        <li>
+          <Link fade to="/Eventos">
+            Eventos
+          </Link>
+        </li>
+        <li id={navbarStyles.suscribirseBoletin}>
+          <Link fade to="/SuscribirseBoletin">
+            <img src={boletinSVG} alt="Suscríbete a nuestro boletín." />
+            <span className={navbarStyles.glowText}>¡Suscríbete al boletín!</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
