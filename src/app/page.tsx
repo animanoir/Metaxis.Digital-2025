2def53232d5bf39730116c4392b7b7b9047f89aa
oscaramontiel <etherealflux94@gmail.com>
2025-10-28 22:23:30 -0600

    Remove ArenaContent component from Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-import ArenaContent from '@/components/arenaContent/ArenaContent';
 import BlogListing from '@/components/blogListing/BlogListing'
 import BooksSliderWrapper from '@/components/bookSlider';
 import LatestEventsGrid from '@/components/latestEventsGrid/LatestEventsGrid';
 
 export default function Home() {
   return (
-    <>
+    <div className="px-6 py-12 md:px-12 lg:px-24 xl:px-32 space-y-16 md:space-y-20 lg:space-y-24">
       <BlogListing />
       <BooksSliderWrapper />
       <LatestEventsGrid />
-      <ArenaContent />
-    </>
+    </div>
   );
 }
